88327ad898b4b6e8dc9a2a1674f14fb9995cd125
Anu Bala <anu@mabzone.com>
2020-09-23 19:24:30 +0530

    V1 -> modification by Anu
    treating binary as string. using JS strign function to read binary data of previous, next and to join. like substr, concat etc

--- VoBoNodeDecoder.js
+++ VoBoNodeDecoder.js
@@ -1,20 +1,18 @@
-function Decoder(bytes, port) {
-  // Decode an uplink message from a buffer
-  // (array) of bytes to an object of fields.
+function Decoder1(bytes, port) {
+            // Decode an uplink message from a buffer
+            // (array) of bytes to an object of fields.
+            var decoded = {};
+            decoded.DIN1 = bytes[0] >> 0 & 1;
+            decoded.DIN2 = bytes[0] >> 1 & 1;
+            decoded.DIN3 = bytes[0] >> 2 & 1;
+            decoded.WKUP = bytes[0] >> 3 & 1;
+            decoded.ADC1 = parseInt((bytes[1] + bytes[0].substr(0, 4)), 2);
+            decoded.ADC2 = parseInt((bytes[3].substr(4, 8) + bytes[2]), 2);
+            decoded.ADC3 = parseInt((bytes[4] + bytes[3].substr(0, 4)), 2);
+            decoded.Battery = (parseInt((bytes[6].substr(4, 8) + bytes[5]), 2)) * 4; // ADC4
+            decoded.Temperature = ((parseInt((bytes[7].concat(bytes[6].substr(0, 4))), 2)) * 0.125 * 9) / 5 + 32;
+            decoded.Modbus = parseInt((bytes[9].concat(bytes[8])), 2);
+            decoded.CRC8 = parseInt(bytes[10], 2);
 
-  var decoded = {};
-
-  decoded.DIN1 = bytes[0] >> 0 & 1;
-  decoded.DIN2 = bytes[0] >> 1 & 1;
-  decoded.DIN3 = bytes[0] >> 2 & 1;
-  decoded.WKUP = bytes[0] >> 3 & 1;
-  decoded.ADC1 = parseInt(("00000000".substr(bytes[1].toString(2).length) + bytes[1].toString(2)+("00000000".substr(bytes[0].toString(2).length) + bytes[0].toString(2)).substr(0,4)),2);
-  decoded.ADC2 = parseInt(("00000000".substr(bytes[3].toString(2).length) + bytes[3].toString(2)).substr(4,8)+("00000000".substr(bytes[2].toString(2) .length) + bytes[2].toString(2)),2);
-  decoded.ADC3 = parseInt(("00000000".substr(bytes[4].toString(2).length) + bytes[4].toString(2)+("00000000".substr(bytes[3].toString(2).length) + bytes[3].toString(2)).substr(0,4)),2);
-  decoded.Battery = (parseInt(("00000000".substr(bytes[6].toString(2).length) + bytes[6].toString(2)).substr(4,8)+("00000000".substr(bytes[5].toString(2) .length) + bytes[5].toString(2)),2)) * 4;
-  decoded.Temperature = ((parseInt(("00000000".substr(bytes[7].toString(2).length) + bytes[7].toString(2)+("00000000".substr(bytes[6].toString(2).length) + bytes[6].toString(2)).substr(0,4)),2)) * 0.125 *9)/5+32;
-  decoded.Modbus = parseInt(("00000000".substr(bytes[9].toString(2).length) + bytes[9].toString(2))+("00000000".substr(bytes[8].toString(2).length) + bytes[8].toString(2)),2);
-  decoded.CRC8 = parseInt(("00000000".substr(bytes[10].toString(2).length) + bytes[10].toString(2)),2);
-
-  return decoded;
-}
+            return decoded;
+        }
