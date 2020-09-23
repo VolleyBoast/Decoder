9012572b3b1b2fa6e49ce0f7d6b09b9cc64c3d09
Anu Bala <anu@mabzone.com>
2020-09-23 19:26:07 +0530

    V2 -> Modification by Anu
    using bit shifting, removed js string functions to make this more robust and flexible.

--- VoBoNodeDecoder.js
+++ VoBoNodeDecoder.js
@@ -1,18 +1,16 @@
-function Decoder1(bytes, port) {
-            // Decode an uplink message from a buffer
-            // (array) of bytes to an object of fields.
+//var bytes= [0b11110011, 0b00110000, 0b00111111, 0b00010011, 0b00000000, 0b01001010, 0b00100011, 0b00001011, 0b11010010, 0b00000100, 0b01011111];
+function Decoder(bytes, port) {
             var decoded = {};
-            decoded.DIN1 = bytes[0] >> 0 & 1;
-            decoded.DIN2 = bytes[0] >> 1 & 1;
-            decoded.DIN3 = bytes[0] >> 2 & 1;
-            decoded.WKUP = bytes[0] >> 3 & 1;
-            decoded.ADC1 = parseInt((bytes[1] + bytes[0].substr(0, 4)), 2);
-            decoded.ADC2 = parseInt((bytes[3].substr(4, 8) + bytes[2]), 2);
-            decoded.ADC3 = parseInt((bytes[4] + bytes[3].substr(0, 4)), 2);
-            decoded.Battery = (parseInt((bytes[6].substr(4, 8) + bytes[5]), 2)) * 4; // ADC4
-            decoded.Temperature = ((parseInt((bytes[7].concat(bytes[6].substr(0, 4))), 2)) * 0.125 * 9) / 5 + 32;
-            decoded.Modbus = parseInt((bytes[9].concat(bytes[8])), 2);
-            decoded.CRC8 = parseInt(bytes[10], 2);
-
+            decoded.DIN1 = bytes[0] & 0x01;
+            decoded.DIN2 = bytes[0] >> 1 & 0x01;
+            decoded.DIN3 = bytes[0] >> 2 & 0x01;
+            decoded.WKUP = bytes[0] >> 3 & 0x01;
+            decoded.ADC1 = ((bytes[0] & 0xf0) >> 4) | (bytes[1] << 4);
+            decoded.ADC2 = (bytes[3] & 0x0f) << 8 | bytes[2];
+            decoded.ADC3 = ((bytes[3] & 0xf0) >> 4) | (bytes[4] << 4);
+            decoded.Battery = ((bytes[6] & 0x0f) << 8 | bytes[5]) * 4; // ADC4
+            decoded.Temperature = ((((bytes[6] & 0xf0) >> 4) | (bytes[7] << 4)) * 0.125 * 9) / 5 + 32;
+            decoded.Modbus = bytes[9] << 8 | bytes[8];
+            decoded.CRC8 = bytes[10];
             return decoded;
         }
