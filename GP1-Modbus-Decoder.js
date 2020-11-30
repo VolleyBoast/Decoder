bbdc5905c6f8b707332f75a0cd2aaa3c944d3cb2
Evan Wood <33100255+ejwwelshboy@users.noreply.github.com>
2020-11-30 11:50:42 -0600

    Fixed Typo

--- GP1-Modbus-Decoder.js
+++ GP1-Modbus-Decoder.js
@@ -5,10 +5,11 @@ function Decoder(bytes, port) {
             decoded.DIN2 = bytes[0] >> 1 & 0x01; // Discrete digital 2 (1-bit)
             decoded.DIN3 = bytes[0] >> 2 & 0x01; // Discrete digital 3 (1-bit)
             decoded.WKUP = bytes[0] >> 3 & 0x01; // Discrete digital wakeup (1-bit)
-            decoded.Modbus1 = bytes[2] << 8 | bytes[1]; // Modbus-RS485 (16-bit)
-            decoded.Modbus2 = bytes[4] << 8 | bytes[3]; // Modbus-RS485 (16-bit)
-            decoded.Modbus3 = bytes[6] << 8 | bytes[5]; // Modbus-RS485 (16-bit)
-            decoded.Modbus4 = bytes[8] << 8 | bytes[7]; // Modbus-RS485 (16-bit)
+            decoded.BatteryLevel = (((bytes[0] & 0xf0) >> 4) | (bytes[1] << 4)) * 4; // ADC battery (12-bit)
+            decoded.Modbus1 = bytes[3] << 8 | bytes[2]; // Modbus-RS485 (16-bit)
+            decoded.Modbus2 = bytes[5] << 8 | bytes[4]; // Modbus-RS485 (16-bit)
+            decoded.Modbus3 = bytes[7] << 8 | bytes[6]; // Modbus-RS485 (16-bit)
+            decoded.Modbus4 = bytes[9] << 8 | bytes[8]; // Modbus-RS485 (16-bit)
             decoded.CRC8 = bytes[10]; // CRC (8-bit)
             return decoded;
         }
