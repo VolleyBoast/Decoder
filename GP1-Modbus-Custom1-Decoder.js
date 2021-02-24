089cbcf4e21404987368dbc6dfa5634c3531a188
Evan Wood <33100255+ejwwelshboy@users.noreply.github.com>
2021-02-24 09:32:38 -0600

    Fixed Alignment Issue

--- GP1-Modbus-Custom1-Decoder.js
+++ GP1-Modbus-Custom1-Decoder.js
@@ -5,10 +5,10 @@ function Decoder(bytes, port) {
             decoded.DIN2 = bytes[0] >> 1 & 0x01; // Discrete digital 2 (1-bit)
             decoded.DIN3 = bytes[0] >> 2 & 0x01; // Discrete digital 3 (1-bit)
             decoded.WKUP = bytes[0] >> 3 & 0x01; // Discrete digital wakeup (1-bit)
-			decoded.BatteryLevel = (((bytes[0] & 0xf0) >> 4) | (bytes[1] << 4)) * 4; // ADC battery (12-bit)
-			decoded.Modbus1 = bytes[3] << 8 | bytes[2]; // Modbus-RS485 (16-bit)
-			decoded.Modbus2 = bytes[5] << 8 | bytes[4]; // Modbus-RS485 (16-bit)
-			decoded.Modbus3 = (bytes[7] << 24 | bytes[6] << 16 | bytes[9] << 8 | bytes[8]) >>> 0; // Modbus-RS485 (32-bit)
+	    decoded.BatteryLevel = (((bytes[0] & 0xf0) >> 4) | (bytes[1] << 4)) * 4; // ADC battery (12-bit)
+	    decoded.Modbus1 = bytes[3] << 8 | bytes[2]; // Modbus-RS485 (16-bit)
+	    decoded.Modbus2 = bytes[5] << 8 | bytes[4]; // Modbus-RS485 (16-bit)
+	    decoded.Modbus3 = (bytes[7] << 24 | bytes[6] << 16 | bytes[9] << 8 | bytes[8]) >>> 0; // Modbus-RS485 (32-bit)
             decoded.CRC8 = bytes[10]; // CRC (8-bit)
             return decoded;
 }
